e45ee021bcdcfa1f8df12f3169bc77c3514efbc3
blake <blakelmason@gmail.com>
2020-08-08 16:14:50 -0700

    lazy

--- cover-letter/src/Components/Right.js
+++ cover-letter/src/Components/Right.js
@@ -1,64 +1,63 @@
 import React from 'react'
 import Title from './Title'
 
-const data = {
-    position: 'Associate Software Engineer',
-    company: 'ProDIGIQ'
-}
-// const test = blah
-
 function Right() {
-    return (
-        <div
-            style={{
-                borderLeft: '1px solid #4c5867',
-                paddingLeft: 24,
-                fontSize: 14,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between'
-            }}
-        >
-            <Title left>Cover Letter</Title>
-            <div style={{}}>Dear Hiring Manager,</div>
-            <P>
-                I am interested in obtaining employment as an {data.position} at {data.company}. I believe my
-                dependable, personable, and professional nature makes me a competitive candidate for this position. I
-                became a software engineer because I love code, and I believe my skills will help me succeed as a{' '}
-                {data.position}.
-            </P>
-            <P>
-                Most recently I worked on three software projects while attending Hack Reactor: a front end capstone, a
-                system design capstone, and a full stack final project. Each project required rigorous teamwork and
-                collaboration while using real world, production grade practices and coding standards. Because I have
-                prior experience working with JavaScript frameworks, I helped teach and guide each team I worked on.
-                From forming an optimal toolchain, to managing git workflows, or simply providing words of
-                encouragement, I was personally involved with each member while also providing my own technical
-                contributions to the project.
-            </P>
-            <P>
-                I am confident my skills as a {data.position} will be valuable to {data.company}, and I would like to
-                further discuss my qualifications. Please feel free to contact me by email at blakelmason@gmail.com or
-                by phone at (714) 878-5117.
-            </P>
-            <div style={{ marginBottom: 14 }}>Sincerely,</div>
-            <div>Blake Mason</div>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        borderLeft: '1px solid #4c5867',
+        paddingLeft: 24,
+        fontSize: 14,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Title left>Cover Letter</Title>
+      <div style={{}}>Hello!</div>
+      <P>
+        I am a software engineer who loves code. After college I thought I would do something with finance or business, but then
+        my older brother introduced me to JavaScript. This opened me up to the world of coding, and I dove in. That was three
+        years ago and my only regret is that I didn't find out about coding sooner! Since then I have eaten up every opportunity
+        I could find to learn more and work toward a professional career in with coding.
+      </P>
+      <P>
+        Currently I work at a marketing company building websites, but I still continue my glorious quest for a place on a team
+        of ardent coders like myself. Recently I completed the Software Engineering Immersive at Hack Reactor in Los Angeles,
+        where I worked with others in a simulated professional coding environment. We worked six days a week from 9AM to 9PM to
+        build fully functional apps from scratch. It was wonderful, and I hope to find a job with a team that follows the same
+        principles I learned at Hack Reactor: to work hard, have fun, and be empathetic in our attitude and communication toward
+        one another.
+      </P>
+      <P>
+        My time spent coding is not exclusive to my job or educational opportunities, I spend my free time coding as well! I
+        love learning new principles, or exploring a new tool or library. I enjoy learning code from others, or teaching someone
+        else what I know whenever I can.
+      </P>
+      <P>
+        To be frank, I have applied to lots of coding jobs and read lots of requirements. Most of the time my skills partialy
+        fit the requirements, but with the Software Developer Intern position at BlenderBotttle I think my skills are a perfect
+        fit. I believe that if I became a part of the team, there would be no doubt or regret in the decision to bring me on.
+      </P>
+      <P>I hope to hear back from you soon!</P>
+      <div style={{ marginBottom: 14 }}>Sincerely,</div>
+      <div>Blake Mason</div>
+    </div>
+  )
 }
 
 function P({ children }) {
-    return (
-        <div
-            style={{
-                lineHeight: 1.5,
-                textJustify: 'inter-word',
-                textAlign: 'justify'
-            }}
-        >
-            {children}
-        </div>
-    )
+  return (
+    <div
+      style={{
+        lineHeight: 1.5,
+        textJustify: 'inter-word',
+        textAlign: 'justify',
+      }}
+    >
+      {children}
+    </div>
+  )
 }
 
 export default Right
