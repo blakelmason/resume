6ad5427b7b2156856b59afa46d385e53e0598437
blake <blakelmason@gmail.com>
2020-08-17 13:52:58 -0700

    lazy

--- cover-letter/src/Components/Right.js
+++ cover-letter/src/Components/Right.js
@@ -21,23 +21,15 @@ function Right() {
         years ago and my only regret is that I didn't find out about coding sooner! Since then I have eaten up every opportunity
         I could find to learn more and work toward a professional career in with coding.
       </P>
+      I recently moved to Utah for financial reasons, and I still continue my glorious quest for a place on a team of ardent
+      coders like myself. Recently I completed the Software Engineering Immersive at Hack Reactor in Los Angeles, where I worked
+      with others in a simulated professional coding environment. We worked six days a week from 9AM to 9PM to build fully
+      functional apps from scratch. It was wonderful, and I hope to find a job with a team that follows the same principles I
+      learned at Hack Reactor: to work hard, have fun, and be empathetic in our attitude and communication toward one another.
       <P>
-        Currently I work at a marketing company building websites, but I still continue my glorious quest for a place on a team
-        of ardent coders like myself. Recently I completed the Software Engineering Immersive at Hack Reactor in Los Angeles,
-        where I worked with others in a simulated professional coding environment. We worked six days a week from 9AM to 9PM to
-        build fully functional apps from scratch. It was wonderful, and I hope to find a job with a team that follows the same
-        principles I learned at Hack Reactor: to work hard, have fun, and be empathetic in our attitude and communication toward
-        one another.
-      </P>
-      <P>
-        My time spent coding is not exclusive to my job or educational opportunities, I spend my free time coding as well! I
-        love learning new principles, or exploring a new tool or library. I enjoy learning code from others, or teaching someone
-        else what I know whenever I can.
-      </P>
-      <P>
-        To be frank, I have applied to lots of coding jobs and read lots of requirements. Most of the time my skills partialy
-        fit the requirements, but with the Software Developer Intern position at BlenderBotttle I think my skills are a perfect
-        fit. I believe that if I became a part of the team, there would be no doubt or regret in the decision to bring me on.
+        I believe that if I became a part of the team at PassiveLogic, there would be no doubt or regret in the decision to
+        bring me on. My positive attitude, resourcefulness, and stubborn perseverance when facing a challenge make me a
+        productive contributor to any professional team.
       </P>
       <P>I hope to hear back from you soon!</P>
       <div style={{ marginBottom: 14 }}>Sincerely,</div>
