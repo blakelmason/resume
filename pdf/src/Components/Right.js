7684b9dfcd55ffb3c46f57e7b0f60aafa0c36ede
Blake Mason <blakelmason@gmail.com>
2020-10-26 15:19:06 -0600

    lazy

--- pdf/src/Components/Right.js
+++ pdf/src/Components/Right.js
@@ -5,15 +5,25 @@ import Experience from './Experience'
 const experiences = [
   {
     description: 'Full service digital sales and marketing agency',
+    href: 'https://lendio.com/',
+    company: 'Lendio',
+    position: 'Software Engineer',
+    date: '2020',
+    data: [
+      `Build UI components and pages for Lendio website and applications.`
+    ],
+  },
+  {
+    description: 'Full service digital sales and marketing agency',
     href: 'https://vonazon.com/',
     company: 'Vonazon',
     position: 'Web Developer',
     date: '2020',
     data: [
-      'Built websites for clients using the HubSpot CMS.',
-      'Translated design and styling guidelines from the Graphics Design team to attractive, responsive web pages.',
-      'Generated custom Bootstrap themes built from the Sass source files using node-sass.',
-      'Created and updated custom templates and modules in HubSpot using the Hubl templating language and Preact, a performant React alternative.',
+      `Built websites for clients using the HubSpot CMS.
+      Translated design and styling guidelines from the Graphics Design team to attractive, responsive web pages.
+      Generated custom Bootstrap themes built from the Sass source files using node-sass.
+      Created and updated custom templates and modules in HubSpot using the Hubl templating language and Preact, a performant React alternative.`
     ],
   },
   {
@@ -22,8 +32,8 @@ const experiences = [
     position: 'software engineer',
     date: '2017 - 2019',
     data: [
-      'Wrote code to automate business operations such as order entry, invoicing, reporting, and generating work order bill of materials.',
-      'Helped troubleshoot any IT problems for the office and warehouse.',
+      `Wrote code to automate business operations such as order entry, invoicing, reporting, and generating work order bill of materials.
+      Helped troubleshoot any IT problems for the office and warehouse.`
     ],
     description: 'Steel wheel assembly and tire distribution',
   },
@@ -36,9 +46,9 @@ const educations = [
     position: 'Software Engineering Immersive',
     date: '2020',
     data: [
-      'Worked on mastering full stack JavaScript and Computer Science fundamentals while exploring new tech using the most refined, up-to-date curriculum.',
-      'Hosted a front end  project on AWS EC2 free tier instances and designed a system to maximize application load.',
-      'Split the proxy, services, and database to individual EC2 instances and configured a load balancer using NGINX.',
+      `Worked on mastering full stack JavaScript and Computer Science fundamentals while exploring new tech using the most refined, up-to-date curriculum.
+      Hosted a front end  project on AWS EC2 free tier instances and designed a system to maximize application load.
+      Split the proxy, services, and database to individual EC2 instances and configured a load balancer using NGINX.`
     ],
     description:
       'An advanced coding bootcamp focused on building autonomous software engineers ready for any job in the tech industry',
